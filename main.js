7c7fc971de339a4cf765544528559ccabaa3fdc1
destroyerman <destroyerman.dev@proton.me>
2024-02-20 15:58:27 +0100

    v1 done

--- main.js
+++ main.js
@@ -4,6 +4,9 @@ import vertex from "./shaders/vertex.glsl?raw";
 import fragment from "./shaders/fragment.glsl?raw";
 import GUI from "lil-gui";
 import Stats from "./Stats";
+import studio from "@theatre/studio";
+import { getProject, types } from "@theatre/core";
+import projectState from "./projectState.json";
 
 class Experience {
     constructor(canvas) {
@@ -12,6 +15,9 @@ class Experience {
         this.canvas = canvas;
 
         this.scene = new THREE.Scene();
+        this.scene.background = new THREE.Color("#b5e48c");
+        this.scene2 = new THREE.Scene();
+        this.scene2.background = new THREE.Color("lightblue");
 
         this.setRenderer();
         this.setMesh();
@@ -23,29 +29,70 @@ class Experience {
     }
 
     setMesh() {
-        this.mesh = new THREE.Mesh(
-            new THREE.BoxGeometry(1, 2, 1),
-            new THREE.MeshStandardMaterial({
-                color: "hotpink",
-            })
-        );
-
-        this.mesh.castShadow = true;
-        this.mesh.receiveShadow = true;
-
-        this.scene.add(this.mesh);
+        /**
+         * Scene 1
+         */
 
         this.ground = new THREE.Mesh(
             new THREE.CircleGeometry(5, 48),
-            new THREE.MeshStandardMaterial({ color: "white" })
+            new THREE.MeshStandardMaterial({ color: "#99d98c" })
         );
 
         this.ground.position.y = -1;
         this.ground.rotation.x = -Math.PI / 2;
-
         this.ground.receiveShadow = true;
 
         this.scene.add(this.ground);
+
+        this.sphere = new THREE.Mesh(
+            new THREE.SphereGeometry(1, 64, 64),
+            new THREE.MeshStandardMaterial({ color: "#99d98c" })
+        );
+
+        this.scene.add(this.sphere);
+
+        /**
+         * Portal
+         */
+        this.portal = new THREE.Mesh(
+            new THREE.CircleGeometry(3, 64),
+            new THREE.ShaderMaterial({
+                vertexShader: vertex,
+                fragmentShader: fragment,
+                uniforms: {
+                    tDiffuse: { value: null },
+                    uResolution: {
+                        value: new THREE.Vector2(
+                            window.innerWidth,
+                            window.innerHeight
+                        ),
+                    },
+                },
+            })
+        );
+
+        this.portal.position.z = -5;
+        this.portal.position.y = 1;
+        this.scene.add(this.portal);
+
+        /**
+         * Scene 2
+         */
+
+        this.ground2 = new THREE.Mesh(
+            new THREE.CircleGeometry(5, 48),
+            new THREE.MeshStandardMaterial({ color: "skyblue" })
+        );
+
+        this.ground2.position.y = -1;
+        this.ground2.position.z = -20;
+        this.ground2.rotation.x = -Math.PI / 2;
+        this.ground2.receiveShadow = true;
+
+        this.scene2.add(this.ground2);
+
+        this.sphere2 = this.sphere.clone();
+        this.scene2.add(this.sphere2);
     }
 
     setCamera() {
@@ -55,15 +102,15 @@ class Experience {
             0.1,
             100
         );
-        this.camera.position.set(0, 2, 5);
+        this.camera.position.set(0, 0, 5);
 
-        const controls = new OrbitControls(this.camera, this.canvas);
-        controls.enableDamping = true;
-
-        this.updateControls = function (deltaTime) {
-            controls.dampingFactor = deltaTime * 5;
-            controls.update();
-        };
+        this.camera2 = new THREE.PerspectiveCamera(
+            75,
+            window.innerWidth / window.innerHeight,
+            0.1,
+            100
+        );
+        this.camera2.position.set(0, 0, 5);
     }
 
     setLights() {
@@ -73,11 +120,21 @@ class Experience {
         this.dir.shadow.mapSize = new THREE.Vector2(1024 * 2, 1024 * 2);
         this.scene.add(this.dir);
 
-        this.dirHelp = new THREE.CameraHelper(this.dir.shadow.camera, 1, "red");
-        // this.scene.add(this.dirHelp);
-
         this.ambient = new THREE.AmbientLight();
         this.scene.add(this.ambient);
+
+        /**
+         * Scene 2
+         */
+
+        this.dir2 = new THREE.DirectionalLight();
+        this.dir2.position.set(1.76, 2.26, 2.38);
+        this.dir2.castShadow = true;
+        this.dir2.shadow.mapSize = new THREE.Vector2(1024 * 2, 1024 * 2);
+        this.scene2.add(this.dir2);
+
+        this.ambient2 = new THREE.AmbientLight();
+        this.scene2.add(this.ambient2);
     }
 
     setEvents() {
@@ -98,10 +155,14 @@ class Experience {
             antialias: true,
         });
         this.renderer.setSize(window.innerWidth, window.innerHeight);
-        this.renderer.setClearColor("black");
         this.renderer.shadowMap.enabled = true;
-        this.renderer.toneMapping = THREE.ACESFilmicToneMapping;
         this.renderer.toneMappingExposure = 1;
+
+        this.renderTarget = new THREE.WebGLRenderTarget(
+            window.innerWidth,
+            window.innerHeight,
+            { samples: 3 }
+        );
     }
 
     setTick() {
@@ -128,11 +189,21 @@ class Experience {
         this.deltaTime = this.elapsedTime - this.previousTime;
         this.previousTime = this.elapsedTime;
 
-        this.renderer.render(this.scene, this.camera);
+        this.renderer.setRenderTarget(this.renderTarget);
+        this.renderer.render(this.scene2, this.camera);
 
-        this.mesh.rotation.y += this.deltaTime;
+        this.portal.material.uniforms.tDiffuse.value =
+            this.renderTarget.texture;
+        this.renderer.setRenderTarget(null);
 
-        this.updateControls(this.deltaTime);
+        if (this.camera.position.z < -3.5) {
+            this.renderer.render(this.scene2, this.camera);
+        } else {
+            this.renderer.render(this.scene, this.camera);
+        }
+
+        this.sphere.position.set(0, 0, this.camera.position.z - 8);
+        this.sphere2.position.set(0, 0, this.camera.position.z - 8);
 
         this.stats.end();
         requestAnimationFrame(this.tick);
@@ -140,6 +211,28 @@ class Experience {
 
     setDebug() {
         const gui = new GUI();
+
+        studio.initialize();
+
+        // Create a project for the animation
+        const project = getProject("THREE.js x Theatre.js", {
+            state: projectState,
+        });
+
+        // Create a sheet
+        const sheet = project.sheet("Animated scene");
+
+        const cameraObj = sheet.object("Camera", {
+            position: { z: this.camera.position.z },
+            rotation: types.compound({
+                x: types.number(0, { range: [-2, 2] }),
+            }),
+        });
+
+        cameraObj.onValuesChange(({ position, rotation }) => {
+            this.camera.position.z = position.z;
+            this.camera.rotation.x = rotation.x;
+        });
     }
 }
 
--- shaders/fragment.glsl
+++ shaders/fragment.glsl
@@ -1,11 +1,18 @@
 uniform float uTime;
-// uniform vec4 uResolution;
+uniform sampler2D tDiffuse;
+uniform vec2 uResolution;
 
 varying vec2 vUv;
 varying vec3 vPosition;
 
-void main() {
-    // vec2 uv = (vUv - vec2(0.5)) * uResolution.zw + vec2(0.5);
 
-    gl_FragColor = vec4(vUv, 0.0, 1.0);
+void main() {
+    vec2 uv = gl_FragCoord.xy / uResolution.xy;
+
+    vec4 text = texture2D(tDiffuse, uv);
+
+    gl_FragColor = text;
+
+	// color space
+	gl_FragColor =  sRGBTransferOETF( gl_FragColor );
 }
